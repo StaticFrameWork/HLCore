6de34626ce726ac3873b5727e9725201fd5faf54
Havi.X.Li.-ND@disney.com <>
2018-11-13 11:22:47 +0800

    initial

---
+++ HLCore/HLCore.h
@@ -0,0 +1,19 @@
+//
+//  HLCore.h
+//  HLCore
+//
+//  Created by Li, Havi X. -ND on 2018/11/13.
+//  Copyright © 2018 Li, Havi X. -ND. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HLCore.
+FOUNDATION_EXPORT double HLCoreVersionNumber;
+
+//! Project version string for HLCore.
+FOUNDATION_EXPORT const unsigned char HLCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HLCore/PublicHeader.h>
+
+
---
+++ HLCore/HLCore.swift
@@ -0,0 +1,13 @@
+//
+//  HLCore.swift
+//  HLCore
+//
+//  Created by Li, Havi X. -ND on 2018/11/13.
+//  Copyright © 2018 Li, Havi X. -ND. All rights reserved.
+//
+
+import Foundation
+
+class HLClass: NSObject {
+	
+}
